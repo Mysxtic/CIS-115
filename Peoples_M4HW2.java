d25a3b6d1166d2382cf98a2fd676c296da892639
Mysxtic <dayvionpeoples4@gmail.com>
2024-10-28 14:31:13 -0400

    Add files via upload

---
+++ Peoples_M4HW2.java
@@ -0,0 +1,23 @@
+public class Peoples_M4HW2 
+{
+
+    public static void main(String[] args) 
+    {
+        String age, citizen, 
+Display “Are you 18 years or older? Enter yes or no “
+Input Age:
+If(age.equalsignorecase(“ yes "))
+        System.out.println(“You passed the age rule. You are eligible to vote. “);
+If (citizen.equalsignorecase(“yes”))
+        System.out.println(“You passed the age rule. You are eligible to vote. “);
+
+Else
+        System.out.println(“You cannot vote due to citizenship rule”)
+
+
+
+Else
+        System.out.println(“You cannot vote due to age rule”)
+
+    }
+}
