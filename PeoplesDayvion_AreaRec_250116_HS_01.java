8bf24532d23ba9ab6dca612dca36ccc601cba08f
Mysxtic <dayvionpeoples4@gmail.com>
2025-01-16 14:42:32 -0500

    Add files via upload

---
+++ PeoplesDayvion_AreaRec_250116_HS_01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package peoplesdayvion_arearec_250116_hs_01;
+
+/**
+ *
+ * @author peoplesd0779
+ */
+public class PeoplesDayvion_AreaRec_250116_HS_01
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
