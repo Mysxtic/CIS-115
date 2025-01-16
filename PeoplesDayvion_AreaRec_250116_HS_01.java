e6f6023278d03c25edbe18133cfcda12b4ae9484
Mysxtic <dayvionpeoples4@gmail.com>
2025-01-16 14:43:22 -0500

    Add files via upload

--- PeoplesDayvion_AreaRec_250116_HS_01.java
+++ PeoplesDayvion_AreaRec_250116_HS_01.java
@@ -3,20 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package peoplesdayvion_arearec_250116_hs_01;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author peoplesd0779
  */
 public class PeoplesDayvion_AreaRec_250116_HS_01
 {
-
+public static double length = 0.0 ;
+public static double width;
+public static double area;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+    getLength();
+    getWidth();
+    displayArea();    // TODO code application logic here
     }
     
+    public static void getLength()
+    {
+        length = Double.parseDouble(JOptionPane.showInputDialog("Enter Length"));
+    }
+    public static void getWidth()
+{
+width = Double.parseDouble(JOptionPane.showInputDialog("Enter Width"));
+}
+public static void displayArea()
+{
+area = length * width
+        
 }
